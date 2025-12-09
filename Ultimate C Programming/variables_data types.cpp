355393367a2fe64febf846116489f31fd5a08003
arehmanraiskhan-006 <arehmanraiskhan@gmail.com>
2025-12-09 21:06:10 +0500

    Add files via upload

---
+++ Ultimate C Programming/variables_data types.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    a = 6;
+    printf("%d\n", a);
+
+    float b = 1.4;
+    printf("%f\n", b);
+
+    char c = 'A';
+    printf("%c\n", c);
+
+    return 0;
+}
+
