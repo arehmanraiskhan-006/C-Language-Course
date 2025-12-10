758371356dd5a215238ac51bb5ba84238fc287ba
arehmanraiskhan-006 <arehmanraiskhan@gmail.com>
2025-12-10 21:36:50 +0500

    Add files via upload

---
+++ Ultimate C Programming/practice variables/pract prog 3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int c = 37;
+    float f;
+
+    f = (9.0/5.0) * c + 32;
+
+    printf("The temp in fahrenheit is %f", f);
+
+    return 0;
+}
+
---
+++ Ultimate C Programming/practice variables/practice prog.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	// area of rectangle
+	//int length = 34;
+	//int breadth = 66;
+	int length, breadth;
+	printf("Enter length\n");
+	scanf("%d", &length);
+	printf("Enter breadth\n");
+	scanf("%d", &breadth);
+	printf("The Area of Rectangle is: %d", length*breadth);
+	return 0;
+}
